4f6c5a4135b7f26d91ca8f3e06a784ec0faa8381
certified-svg <jboyking44@gmail.com>
2024-09-28 16:50:29 -0500

    Create ANIME LIST.py

---
+++ ANIME LIST.py
@@ -0,0 +1,127 @@
+Top 50 Anime
+Action
+1. JJK
+2. Chainsaw Man
+3. One Punch Man 
+
+Adventure
+1. One piece 
+2. Hunter x Hunter
+3. Dragon Ball 
+
+Comedy
+1. Spy x Family 
+2. Mob Psycho
+3. Konosuba 
+
+Drama 
+1. classroom of the elite
+2. Date a live 
+3. charlotte 
+
+Fantasy
+1. Overlord
+2. Fairy tail 
+3. Black clover
+
+Romance
+1. My teenage romantic comedy
+2. Alya somethimes hides her feleings in Russian
+3. Darling inf franx
+
+Slice of life
+1. Komi can't communicate 
+2. Clannad
+3. Fruits Basket 
+
+Supernatural
+1. In/spectre
+2. Fate/stay night
+3. Demon Slayer
+
+Horror
+1. Eldien lied
+2. Berserk
+3. tokyo ghoul 
+
+Mystery
+1. Bungou stray dogs
+2. Death Note
+3. Jojos bizzare adventure 
+
+Sci-Fi
+1. Cyberpunk edgerunners
+2. sword art online
+3. steins gate
+
+Mecha
+1. Mobile Gundum
+2. Gurren luagan 
+3. kill la kill 
+
+sports
+1. Blue lock
+2. Hajime no ippo
+3. Kuroko's basketball
+
+shonen
+1. Bleach
+2. My hero acedemia 
+3. Dr. stone
+
+shojo
+1. sailor moon
+2. The irregular at a magic high school
+3. whispers of the heart
+
+isekai
+1. That time I got reicnarnated as a slime
+2. Ghe misfit of devil king 
+3. Eminence in shadow 
+
+Harem
+1. High school dxd
+2. I got a cheat skill in another world 
+3. Danmachi
+
+Reverse harem
+1. Vampire knight
+2. Yona of the dawn
+3. Code realize
+
+Tragedy
+1. Alucard
+2. Devilman crybaby
+3. Code geass
+
+Game
+1. Pokemon
+2. Yu-gi-oh
+3. Beyblade 
+
+Top 25
+One piece 
+naruto 
+Attack on titan
+my hero acedemia 
+One punch Man
+Bleach
+Black clover 
+fairy tail
+Berserk
+Tower of god
+Kaiju No.8
+Hunter x hunter
+Dragon ball
+demon slayer
+Fate/stay\
+jjk
+vinland saga
+that time I got reincarnated 
+Death Note
+Classroom of the elite 
+Kill la kill 
+fire force
+mob psycho
+sailor moon 
+overlord
