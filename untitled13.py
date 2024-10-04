f85ecc54e570fc6e323f0ce7995e650cb89e0561
certified-svg <jboyking44@gmail.com>
2024-10-04 15:52:58 -0500

    Add files via upload

---
+++ untitled13.py
@@ -0,0 +1,79 @@
+new_gen_action = ["JJK", "Chainsaw Man"]
+
+old_gen_action = ["One Punch Man"]
+
+new_gen_adventure = 
+
+old_gen_adventure = ["One piece", "Hunter x Hunter", "Dragon Ball"]
+
+new_gen_comedy = ["Spy x Family"]
+
+old_gen_comedy = ["Mob Psycho", "Konosuba"]
+
+new_gen_drama = 
+
+old_gen_drama = ["classroom of the elite", "Date a live", "charlotte"]
+
+new_gen_fantasy = ["Black Clover"]
+
+old_gen_fantasy = ["Overlord", "Fairy tail"]
+
+new_gen_romance = ["My teenage romantic comedy", "Alya somethimes hides her feelings in Russian"]
+
+old_gen_romance = ["Darling inf franx"]
+
+new_gen_slice_of_life = ["Komi can't communicate"]
+
+old_gen_slice_of_life = ["Clannad", "Fruits Basket"]
+
+new_gen_supernatural = ["Demon Slayer"]
+
+old_gen_supernatural = ["In/spectre", "Fate/stay night"]
+
+new_gen_horror =
+
+old_gen_horror = ["Eldien lied", "Berserk", 'tokyo ghoul"]
+
+new_gen_mystery = ["Jojos bizzare adventure" ]
+
+old_gen_mystery = ["Bungou stray dogs", "Death Note"]
+
+new_gen_sci_fi = ["Cyberpunk edgerunners"]
+
+old_gen_sci_fi = ["sword art online", "steins gate"]
+
+new_gen_mecha =
+
+old_gen_mecha = ["Mobile Gundum", "Gurren luagan", "kill la kill"]
+
+new_gen_sports = ["Blue lock"]
+
+old_gen_sports = ["Hajime no ippo", "Kuroko's basketball"]
+
+new_gen_shonen = ["My hero acedemia", "Dr. stone"]
+
+old_gen_shonen = ["Bleach"]
+
+new_gen_shojo = 
+
+old_gen_shojo = ["sailor moon", "The irregular at a magic high school", "whispers of the heart"]
+
+new_gen_isekai = ["The misfit of devil king", "Eminence in shadow "]
+
+old_gen_isekai = "That time I got reicnarnated as a slime"]
+
+new_gen_harem = ["I got a cheat skill in another world"]
+
+old_gen_harem = ["Danmachi", "High school dxd"]
+
+new_gen_reverse_harem = ["Vampire knight"]
+
+old_gen_reverse_harem = ["Yona of the dawn", "Code realize"]
+
+new_gen_tragedy = 
+
+old_gen_tragedy = ["Alucard", "Devilman crybaby", "Code geass"]
+
+new_gen_game = 
+
+old_gen_game = ["Pokemon", "Yu-gi-oh", " Beyblade"]
