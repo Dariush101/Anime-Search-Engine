f23a99cbbc42dd9db8dbaf9e1535cf9b2d5f74fb
Dariush101 <andrewdariushboujabadi@gmail.com>
2025-01-15 21:11:23 -0600

    Update ANIME LIST.py

--- ANIME LIST.py
+++ ANIME LIST.py
@@ -29,33 +29,215 @@
 
 A
 
-Aho-Girl
-Alderamin on the Sky
-All Out!!
-Alya Sometimes Hides Her Feelings in Russian
-Am I Actually the Strongest?
-Ancient Girl’s Frame
-Ancient Magus' Bride
-And You Thought There Is Never a Girl Online?
-Angel Links
-Angel’s 3Piece!
-Angels of Death
-Ange Vierge
-Aoi Bungaku Series
-Aoashi
-Appleseed XIII
-Appare-Ranman!
-Arakawa Under the Bridge
-Arifureta: From Commonplace to World's Strongest
-Arknights: Perish in Frost
-Astra Lost in Space
-Assassination Classroom
-Assault Lily: Bouquet
-Asteroid in Love
-Attack on Titan
-Attack on Titan: Junior High
-Attack on Titan: Lost Girls
-Aquarion
+
+A Bridge to the Starry Skies - Hoshizora e Kakaru Hashi
+A Centaurs Life
+a-certain-magical-index
+a-certain-scientific-accelerator
+a-certain-scientific-railgun
+a-condition-called-love
+a-couple-of-cuckoos
+a-dark-rabbit-has-seven-lives
+a-destructive-god-sits-next-to-me
+a-galaxy-next-door
+a-girl--her-guard-dog
+a-herbivorous-dragon-of-5000-years-gets-unfairly-villainized
+a-journey-through-another-world-raising-kids-while-adventuring
+a-lull-in-the-sea-nagi-asu-nagi-no-asukara
+a-nobodys-way-up-to-an-exploration-hero
+a-place-further-than-the-universe
+a-playthrough-of-a-certain-dudes-vrmmo-life
+
+a-returners-magic-should-be-special
+
+a-salad-bowl-of-eccentrics
+a-sign-of-affection
+a-terrified-teacher-at-ghoul-school
+a-wind-named-amnesia
+a3
+aachi--ssipak
+abciee-working-diary
+absolute-duo
+acca-13-territory-inspection-dept
+ace-attorney
+ace-of-the-diamond
+acro-trip
+active-raidactors-songs-connection
+adachi-and-shimamura
+aesthetica-of-a-rogue-hero
+africa-salaryman
+after-school-dice-club
+after-school-hanako-kun
+afterlost
+ah-my-buddha
+aharen-san-wa-hakarenai
+ahiru-no-sora
+aho-girl
+ai-tenchi-muyo
+ai-mai-mi-mousou-catastrophe
+ai-mai-mi-surgical-friends
+
+air-master
+aiura
+
+
+akagi
+
+akashic-records-of-bastard-magic-instructor
+
+akebis-sailor-uniform
+
+akibas-trip-the-animation
+
+
+akira
+
+
+akudama-drive
+
+alderamin-on-the-sky
+aldnoahzero
+
+alice--zoroku
+
+
+all-saints-street
+
+
+alya-sometimes-hides-her-feelings-in-russian
+
+am-i-actually-the-strongest
+
+amaim-warrior-at-the-borderline
+
+
+amazing-stranger
+
+ameku-md-doctor-detective
+
+
+
+an-archdemons-dilemma-how-to-love-your-elf-bride
+
+https://www.crunchyroll.com/series/GKEH2GQKX/ancient-girls-frame
+
+https://www.crunchyroll.com/series/GRWE8K5MR/and-you-thought-there-is-never-a-girl-online
+
+https://www.crunchyroll.com/series/GY49PW7QR/angel-beats
+
+
+https://www.crunchyroll.com/series/G24H1NWNJ/angel-cop
+
+
+https://www.crunchyroll.com/series/G6X03VD2Y/angel-heart
+
+https://www.crunchyroll.com/series/G6VN8D53R/angels-3piece
+
+
+
+
+https://www.crunchyroll.com/series/GRVDN485R/angels-of-death
+
+
+
+https://www.crunchyroll.com/series/G6J0QJ32R/angolmois-record-of-mongol-invasion
+
+
+https://www.crunchyroll.com/series/GRDKVQ53Y/anima-yell
+
+
+https://www.crunchyroll.com/series/GQWH0MXPQ/anime-azurlane-slow-ahead
+
+https://www.crunchyroll.com/series/GREX5KEXY/anime-crimes-division
+https://www.crunchyroll.com/series/GRE5KZQN6/anime-de-training-ex
+https://www.crunchyroll.com/series/GRZ43X7DY/anime-kapibarasan
+https://www.crunchyroll.com/series/G65VGQJ86/anime-gataris
+
+https://www.crunchyroll.com/series/GYWE2J9JY/anne-happy
+
+https://www.crunchyroll.com/series/GR19X2G26/anohana-the-flower-we-saw-that-day
+
+https://www.crunchyroll.com/series/GR09X52WR/another
+https://www.crunchyroll.com/series/GEXH3W24Z/another-journey-to-the-west
+
+https://www.crunchyroll.com/series/GRZXDKNVY/anti-magic-academy-the-35th-test-platoon
+
+https://www.crunchyroll.com/series/GJ0H7QGGM/anyway-im-falling-in-love-with-you
+
+https://www.crunchyroll.com/series/G4PH0WX5J/aoashi
+
+https://www.crunchyroll.com/series/GY49PZ78R/aoi-sekai-no-chushin-de
+
+https://www.crunchyroll.com/series/GYMGPXKVY/aokana-four-rhythm-across-the-blue
+https://www.crunchyroll.com/series/GRZXQJK3Y/aooni-the-blue-monster
+https://www.crunchyroll.com/series/G5PHNMW9N/appare-ranman
+https://www.crunchyroll.com/series/G3KHEVDDD/aquarion-myth-of-emotions
+https://www.crunchyroll.com/series/GRP8PGDWR/arakawa-under-the-bridge
+https://www.crunchyroll.com/series/GRE5M1X06/arata-the-legend
+https://www.crunchyroll.com/series/G69VE9W4Y/are-you-lost
+https://www.crunchyroll.com/series/GR2P9ZQ2R/aria-the-animation
+https://www.crunchyroll.com/series/GYZXZ5PM6/aria-the-scarlet-ammo
+https://www.crunchyroll.com/series/G4PH0WXD1/arifureta-from-commonplace-to-worlds-strongest
+https://www.crunchyroll.com/series/GG5H5X3VN/arknights
+https://www.crunchyroll.com/series/GZJH3DWPV/armor-shop-for-ladies--gentlemen
+https://www.crunchyroll.com/series/GRQWDD29R/arp-backstage-pass
+https://www.crunchyroll.com/series/GR9PXZ3J6/arpeggio-of-blue-steel
+https://www.crunchyroll.com/series/GDKHZEDPM/arte
+https://www.crunchyroll.com/series/GG5H5XQMD/as-a-reincarnated-aristocrat-ill-use-my-appraisal-skill-to-rise-in-the-world
+https://www.crunchyroll.com/series/GRJ0K0ZDY/as-miss-beelzebub-likes-it
+
+
+https://www.crunchyroll.com/series/G6793XKZY/ascendance-of-a-bookworm
+
+
+https://www.crunchyroll.com/series/G6MEG24VR/asobi-asobase---workshop-of-fun--
+
+https://www.crunchyroll.com/series/GRE59JGX6/assassination-classroom
+https://www.crunchyroll.com/series/G60X4P8GR/assassins-pride
+
+https://www.crunchyroll.com/series/GEXH3WK20/assault-lily-bouquet
+
+https://www.crunchyroll.com/series/G6NQV5WQ6/astarottes-toy
+
+https://www.crunchyroll.com/series/GRMEJX3VY/asteroid-in-love
+
+https://www.crunchyroll.com/series/GMEHMEWZM/astra-lost-in-space
+
+
+https://www.crunchyroll.com/series/G8DHV78D7/astro-note
+
+
+https://www.crunchyroll.com/series/GEXH3W2Z5/atelier-ryza-ever-darkness--the-secret-hideout-the-animation
+
+
+https://www.crunchyroll.com/series/G1XHJV0EM/atri--my-dear-moments-
+
+
+https://www.crunchyroll.com/series/GR751KNZY/attack-on-titan
+https://www.crunchyroll.com/series/GVDHX8P8Q/attack-on-titan-movies
+
+https://www.crunchyroll.com/series/GR758GE5Y/attack-on-titan-junior-high
+
+https://www.crunchyroll.com/series/G0XHWM11X/ave-mujica---the-die-is-cast--
+https://www.crunchyroll.com/series/G0XHWM11X/ave-mujica---the-die-is-cast--
+
+https://www.crunchyroll.com/series/G79H235XP/aware-meisaku-kun
+https://www.crunchyroll.com/series/GXJHM3GQ7/ayaka
+
+https://www.crunchyroll.com/series/GZJH3D0X5/ayakashi-triangle
+
+https://www.crunchyroll.com/series/G9VHN9P49/azur-lane
+
+
+
+
+
+
+
+
+
+
+
 
 
 
