d938592149096f88866a92031f872398ff81d974
certified-svg <jboyking44@gmail.com>
2025-01-19 00:23:27 -0600

    Add files via upload

---
+++ UPDATED NEW ANIME.py
@@ -0,0 +1,134 @@
+
+# Action Anime Instances
+anime_objects = [
+
+
+    AnimeToGenre("JJK", 17, 1, 2, 1, 8,14,0),
+    AnimeToGenre("Chainsaw Man", 19, 1, 2, 1,8,9,14),
+    AnimeToGenre("One Punch Man", 17, 2, 1,1,3,14,0),
+    AnimeToGenre("One Piece", 17, 2, 1,1,2,14,0),
+    AnimeToGenre("Hunter x Hunter", 17, 2, 2,2,1,14,0),
+    AnimeToGenre("Dragon Ball", 17, 2, 1,1,2,14,0),
+    AnimeToGenre("Dragon Ball Z", 17, 2, 1,1,2,14,0),
+    AnimeToGenre("Dragon Ball GT", 17, 2, 1,1,2,14,0),
+    AnimeToGenre("Dragon Ball Super", 17, 1, 1,1,2,14,0),
+    AnimeToGenre("Dragon Ball Daima", 17, 1, 1,1,2,14,0),
+    AnimeToGenre("Mob Psycho", 17, 1, 1,1,3,8,14),
+    AnimeToGenre("Konosuba", 17, 1, 1,3,5,16,0),
+    AnimeToGenre("Classroom of the Elite", 17, 1, 2,4,5,0,0),
+    AnimeToGenre("Date A Live", 17, 1, 1,6,3,16,17),
+    AnimeToGenre("Charlotte", 17, 1, 2,4,8,7,0),
+    AnimeToGenre("Overlord", 17, 1, 2,5,16,1,2),
+    AnimeToGenre("Fairy Tail", 17, 1, 1,1,2,5,14),
+    AnimeToGenre("Black Clover", 17, 1, 1,1,5,14,0),
+    AnimeToGenre("My Teen Romantic Comedy", 17, 1, 2,6,7,4,0),
+    AnimeToGenre("Alya Sometimes Hides Her Feelings in Russian", 17, 1, 1,6,4,7,0),
+    AnimeToGenre("Darling in the Franxx", 19, 1, 2,6,12,1,11),
+    AnimeToGenre("Komi Can't Communicate", 17, 1, 1,3,7,0,0),
+    AnimeToGenre("Clannad", 17, 1, 2,4,7,6,0),
+    AnimeToGenre("Fruits Basket", 17, 2, 2,4,6,7,0),
+    AnimeToGenre("In/Spectre", 17, 1, 2,8,6,0,0),
+    AnimeToGenre("Fate/Stay Night", 17, 2, 2,1,5,8,0),
+    AnimeToGenre("Demon Slayer", 17, 1, 2,1,8,14,0),
+    AnimeToGenre("Elfen Lied", 19, 2, 2,9,8,4,0),
+    AnimeToGenre("Berserk", 19, 2, 2,1,5,9,4),
+    AnimeToGenre("Tokyo Ghoul", 19, 1, 2,1,9,8,4),
+    AnimeToGenre("Bungou Stray Dogs", 17, 1, 2,1,8,4,0),
+    AnimeToGenre("Death Note", 17, 2, 2,10,8,0,0),
+    AnimeToGenre("JoJo's Bizarre Adventure", 17, 2, 1,2,5,14,0),
+    AnimeToGenre("Cyberpunk: Edgerunners", 19, 1, 2,1,11,4,0),
+    AnimeToGenre("Sword Art Online", 17, 1, 1,1,2,11,16),
+    AnimeToGenre("Steins;Gate", 17, 1, 2,11,33,0,0),
+    AnimeToGenre("Mobile Gundam", 17, 2, 2,12,1,11,0),
+    AnimeToGenre("Gurren Lagann", 17, 2, 1,1,12,11,2),
+    AnimeToGenre("Kill la Kill", 19, 1, 1,1,12,3,0),
+    AnimeToGenre("Blue Lock", 17, 1, 1,13,1,14,0),
+    AnimeToGenre("Hajime no Ippo", 17, 1, 1,13,4,1,0),
+    AnimeToGenre("Kuroko's Basketball", 17, 1, 1,13,1,14,0),
+    AnimeToGenre("Sailor Moon", 17, 2, 1,15,5,6,0),
+    AnimeToGenre("The Irregular at Magic High School", 17, 1, 2,11,1,14,0),
+    AnimeToGenre("Whispers of the Heart", 17, 2, 1,6,7,4,0),
+    AnimeToGenre("That Time I Got Reincarnated as a Slime", 17, 1, 1,16,5,2,0),
+    AnimeToGenre("The Misfit of Demon King Academy", 17, 1, 1,5,16,1,0),
+    AnimeToGenre("Eminence in Shadow", 17, 1, 1,16,1,5,0),
+    AnimeToGenre("High School DxD", 19, 1, 2,1,6,17,8),
+    AnimeToGenre("I Got a Cheat Skill in Another World", 17, 1, 1,16,1,5,17),
+    AnimeToGenre("DanMachi", 17, 1, 1,5,16,1,2),
+    AnimeToGenre("Vampire Knight", 17, 2, 2,18,6,8,4),
+    AnimeToGenre("Yona of the Dawn", 17, 1, 1,18,6,4,5),
+    AnimeToGenre("Code: Realize", 17, 1, 2,18,6,5,0),
+    AnimeToGenre("Alucard", 19, 2, 2,1,8,9,0),
+    AnimeToGenre("Devilman Crybaby", 19, 1, 2,1,8,4,9),
+    AnimeToGenre("Code Geass", 17, 2, 1,1,12,4,11),
+    AnimeToGenre("Pokémon", 17, 2, 1,1,2,20,0),
+    AnimeToGenre("Yu-Gi-Oh", 17, 2, 1,20,1,2,0),
+    AnimeToGenre("Beyblade", 17, 2, 1,20,1,13,0),
+    AnimeToGenre("Solo Leveling")
+    AnimeToGenre("Tower God")
+    AnimeToGenre("Ouran Host Club")
+    AnimeToGenre("DAN DA DAN")
+    AnimeToGenre("Mashle")
+    AnimeToGenre("A Certain Magical Club")
+    AnimeToGenre("Absolute Duo")
+    AnimeToGenre("Afro Samurai")
+    AnimeToGenre("Another")
+    AnimeToGenre("Black Lagoon")
+    AnimeToGenre("Deadman Wonderland")
+    AnimeToGenre("Digimon")
+    AnimeToGenre("Don't Toy With Me, Miss Nagatoro")
+    AnimeToGenre("Edends Zero")
+    AnimeToGenre("Fist Of the Northstar")
+    AnimeToGenre ("Fullmetal Alchemist")
+    AnimeToGenre("Fuuka")
+    AnimeToGenre("Gintama")
+    AnimeToGenre("Girlfriend, Girlfriend")
+    AnimeToGenre("Goblin Slayer")
+    AnimeToGenre("Haikyu")
+    AnimeToGenre("Hell's Paradise")
+    AnimeToGenre("Hellsing")
+    AnimeToGenre("How Not to Summon a Demon Lord")
+    AnimeToGenre("Masamune-kun's Revenge")
+    AnimeToGenre("Megalobox")
+    AnimeToGenre("My First Girlfriend is a Gal")
+    AnimeToGenre("Omamori Himari")
+    AnimeToGenre("OniAi")
+    AnimeToGenre("ORESUKI")
+    AnimeToGenre("Parasyte")
+    AnimeToGenre("Plunderer")
+    AnimeToGenre("Re:Zero")
+    AnimeToGenre("Rent-a-Girlfriend")
+    AnimeToGenre("Rurouni Kenshin")
+    AnimeToGenre("Shimoneta")
+    AnimeToGenre("Shuffle")
+    AnimeToGenre("Skeleton Knight in Another World")
+    AnimeToGenre("Snow White With the Red Hair")
+    AnimeToGenre("Soul Eater")
+    AnimeToGenre("Strike the Blood")
+    AnimeToGenre("The Cafe Terrace and Its Goddesses")
+    AnimeToGenre("The Detective is Already Dead")
+    AnimeToGenre("The Devil is a Part-Timer")
+    AnimeToGenre("The Future Diary")
+    AnimeToGenre("The God of High School")
+    AnimeToGenre("The Kingdoms of Ruin")
+    AnimeToGenre("Tokyo Ravens")
+    AnimeToGenre("Tokyo Revengers")
+    AnimeToGenre("Tomo-chan Is a Girl")
+    AnimeToGenre("Tsugumomo")
+    AnimeToGenre("Tsukimichi, Moonlit Fantasy")
+    AnimeToGenre("Twin Star Exorcists")
+    AnimeToGenre("Vinland Saga")
+    AnimeToGenre("Walkure Romanze")
+    AnimeToGenre("Welcome to Demon School Iruma-kun")
+    AnimeToGenre("When Supernatural Battles Became Commonplace")
+    AnimeToGenre("World's End Harem")
+    AnimeToGenre("Yu Yu Hakusho")
+    AnimeToGenre("Zom 100: Bucket List of the Dead")
+    AnimeToGenre("You are Ms. Servant")
+    AnimeToGenre("Trinity Seven")
+    AnimeToGenre("Trapped in a Data Sim")
+    AnimeToGenre("To the Abandoned Sacred Beasts")
+    AnimeToGenre("The Testament of Sister New Devil")
+    AnimeToGenre("The Rising of the Shield Hero")
+    
+    
+]
